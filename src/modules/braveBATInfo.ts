44992233561bdcd2602e470fe119745e454a59f4
Sampson <jjdsampson@gmail.com>
2024-06-01 15:48:00 -0500

    Changes source of MAU/DAU from external to internal
    Updates are published each month (typically via x.com/brendaneich), and then reflected on the community site bravebat.info. We've been retrieving the MAU and DAU figures from bravebat.info for quite some time, but are now serving the data from our own endpoint. For this reason, we can move closer to complete-automation of these figures by leaning on the internal source, rather than leveraging the community source.

--- src/modules/braveBATInfo.ts
+++ src/modules/braveBATInfo.ts
@@ -1,20 +1,6 @@
 import fetch from "node-fetch";
 import { channelLabels, debugLOG } from "../utils.js";
 
-type MAUDAULabels = "Brave Browser MAU (M)" | "Brave Browser DAU (M)";
-
-interface ActiveUsers {
-  labels: string[];
-  data: {
-    [key in MAUDAULabels]: number[];
-  };
-}
-
-interface ActiveUsersSet {
-  mau: ActiveUsers;
-  dau: ActiveUsers;
-}
-
 interface GrowthRecord {
   record_date: string;
   total: number;
@@ -22,30 +8,6 @@ interface GrowthRecord {
 
 interface CategoryGrowthStats {
   [key: string]: Record<string, number>;
-}
-
-async function getUsersMAU(): Promise<ActiveUsers> {
-  debugLOG("Requesting MAUs from BraveBAT.info");
-  const endpoint = `https://bravebat.info/charts/mau`;
-  const response = (await (await fetch(endpoint)).json()) as ActiveUsers;
-  debugLOG("Retrieved MAUs from BraveBAT.info");
-  return response;
-}
-
-async function getUsersDAU(): Promise<ActiveUsers> {
-  debugLOG("Requesting DAUs from BraveBAT.info");
-  const endpoint = `https://bravebat.info/charts/dau`;
-  const response = (await (await fetch(endpoint)).json()) as ActiveUsers;
-  debugLOG("Retrieved DAUs from BraveBAT.info");
-  return response;
-}
-
-export async function getUsers(): Promise<ActiveUsersSet> {
-  debugLOG("Requesting users from BraveBAT.info");
-  const mau = await getUsersMAU();
-  const dau = await getUsersDAU();
-  debugLOG("Retrieved users from BraveBAT.info");
-  return { mau, dau };
 }
 
 export async function getCreatorGrowth() {
